b91f66e1b62414c78935e72fe730c3bc65def460
mattsluis <Sluismatthew@gmail.com>
2019-08-29 08:59:21 -0700

    styling

--- app/javascript/packs/app/components/omdb/movie_show/movie_show.jsx
+++ app/javascript/packs/app/components/omdb/movie_show/movie_show.jsx
@@ -11,14 +11,25 @@ export default class MovieShow extends Component {
         super(props);
         this.state = {};
     }
+    goBack(){
+        this.props.history.goBack();
+    }
 
     render() {
         const { data } = this.props;
+        const backButtonProps = {
+            buttonText: 'Go Back',
+            classNames: '',
+        }
+        console.log(data)
         return (
             <div className={Style.movieContainer}>
                 <h1 className={Style.movieHeader}>{data.Title}</h1>
+                <span className={Style.stars}>Starring: {data.Actors}</span>
                 <p className={Style.plot}>{data.Plot}</p>
-                <Link to='/'>Home</Link>
+                <Link to='/'>
+                    <Button {...backButtonProps}/>
+                </Link>
             </div>
         )
     }
